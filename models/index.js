3d71310f66fa232a990c4ecc0a12f953e92327c8
geigea84 <geigea84@gmail.com>
2020-12-13 22:17:59 -0600

    created models files, began initial set up on index.js

---
+++ models/index.js
@@ -0,0 +1,9 @@
+//importing models to set up associations and export as objects
+
+const User    = require("./User");
+const Post    = require("./Post");
+const Comment = require("./Comment");
+
+
+
+module.exports = {User, Post, Comment};
